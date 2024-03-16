f9feef6bae12106962a3db3598f50b9caefc2dae
Federicaye <gallofederica58@gmail.com>
2024-03-16 16:18:10 +0100

    d-none

--- resource/script.js
+++ resource/script.js
@@ -1,13 +1,21 @@
 function createSquares(number, className) { /* funzione per creare un numero di quadrati */
-    let squares = [];
-    for (let i = 0; i < number; i++) {
-        let square = document.createElement("div");
+    let squares = []; /* array che contine i div */
+    for (let i = 0; i < number; i++) { /* ciclo che si ripete un number (argomento) di volte */
+        let square = document.createElement("div"); /* variabili che contiene i div */
         let randomContent = Math.random() < 0.5 ? "fiore" : "bomba";
-        square.innerHTML = `<span> ${randomContent}</span>`;
+        /* square.innerHTML = `<span> ${randomContent}</span>`; */
+        let content = document.createElement("span"); /* variabile che contiene gli span */
+        content.textContent = randomContent; 
+        square.appendChild(content);
         square.classList.add("square");
         square.classList.add(className);
+        content.classList.add("d-none");
+        square.addEventListener("click", function () {
+            content.classList.remove("d-none")
+        });
         squares.push(square);
     }
+
     return squares;
 };
 
@@ -34,7 +42,7 @@ play.addEventListener("click", function () {
     if (difficulty.value === "difficoltà1") {
         squares = createSquares(49, "facile");
         console.log(squares);
-        
+
     } else if (difficulty.value === "difficoltà2") {
         squares = createSquares(81, "medio");
         console.log(squares);
@@ -44,7 +52,7 @@ play.addEventListener("click", function () {
     }
     campoMinato.innerHTML = "";
     squares.forEach(square => {
-        campoMinato.appendChild(square); 
+        campoMinato.appendChild(square);
     });
 });
 
