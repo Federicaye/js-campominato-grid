362f8e4fdc175fb26f2d2c868603121f9ae8a0d3
Federicaye <gallofederica58@gmail.com>
2024-03-16 17:59:39 +0100

    funzione

--- resource/script.js
+++ resource/script.js
@@ -2,16 +2,18 @@ function createSquares(number, className) { /* funzione per creare un numero di 
     let squares = []; /* array che contine i div */
     for (let i = 0; i < number; i++) { /* ciclo che si ripete un number (argomento) di volte */
         let square = document.createElement("div"); /* variabili che contiene i div */
-        let randomContent = Math.random() < 0.5 ? "fiore" : "bomba";
+        let randomContent = Math.random() < 0.8 ? "fiore" : "bomba";
         /* square.innerHTML = `<span> ${randomContent}</span>`; */
         let content = document.createElement("span"); /* variabile che contiene gli span */
         content.textContent = randomContent; 
         square.appendChild(content);
         square.classList.add("square");
         square.classList.add(className);
+        square.setAttribute("id", i+1)
         content.classList.add("d-none");
         square.addEventListener("click", function () {
             content.classList.remove("d-none")
+            console.log(square.id);
         });
         squares.push(square);
     }
@@ -51,8 +53,8 @@ play.addEventListener("click", function () {
         console.log(squares);
     }
     campoMinato.innerHTML = "";
-    squares.forEach(square => {
-        campoMinato.appendChild(square);
+    squares.forEach(element => {
+        campoMinato.appendChild(element);
     });
 });
 
